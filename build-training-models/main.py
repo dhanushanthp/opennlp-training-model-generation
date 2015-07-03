b5b3f9527b58e8624f2172a611461f2b185abcce
root <root@dhanu-HP-ProBook-4430s>
2015-07-03 15:30:59 +0530

    fx.

--- build-training-models/main.py
+++ build-training-models/main.py
@@ -1,47 +1,49 @@
-f_in = open("train.txt", "r")  # opens a file in the reading mode
-in_lines = f_in.readlines()  # reads it line by line
+'''
+The application has been developed to change the default training data
+to our own requirement  by replacing some POS tags to build a training
+model.
+'''
+f_in = open("train.txt", "r")
+in_lines = f_in.readlines()
 out = []
 
-i = 0
-while (i < len(in_lines)):
-# for i in range(len(in_lines)):
-	# print (i)
-	if in_lines[i].strip() != '':
-		list_values = in_lines[i].split()  # to remove in IN B-PP
+count = 0
+while (count < len(in_lines)):
+	if in_lines[count].strip() != '':
+		list_values = in_lines[count].split()  # split to get the data pattern as <word> <pos> <train_tag>
 		try:
-			list_values_next = in_lines[i + 1].split()  # to remove in IN B-PP
+			list_values_next = in_lines[count + 1].split()  # to remove in <IN> <B-PP>
 		except Exception as e:
 			print (e)
 		else:
 			pass
 		finally:
 			pass
-	    # print (in_lines[i])
-		print (i)
+		print (count)
 		if list_values[2] == 'B-PP':
 			list_values[2] = 'O'
 			out.append(list_values)
-			i = i + 1
-		elif list_values[1] == 'DT' and list_values[2] == 'B-NP' :  # to remove the DT B-NP
+			count = count + 1
+		elif list_values[1] == 'DT' and list_values[2] == 'B-NP' :  # to remove the <DT> <B-NP>
 			list_values[2] = 'O'
 			out.append(list_values)
-			if list_values_next[2] == 'I-NP' :  # if next is NN I-NP
+			if list_values_next[2] == 'I-NP' :  # if next is <NN> <I-NP> then that need to be changed as <B-NP>. Because B is consider as a starting point of the chunk
 				list_values_next[2] = 'B-NP'
 				out.append(list_values_next)
-				i = i + 1;
-				print ('in ' , i)
-			i = i + 1
+				count = count + 1;
+				print ('in ' , count)
+			count = count + 1
 		else :
 			out.append(list_values)
-			i = i + 1
+			count = count + 1
 	else:
 		out.append("")
-		i = i + 1
+		count = count + 1
 f_in.close() 
 
 f_out = open("en-chunker.train", "w")  # opens a new file in the writing mode
 for cur_list in out:
-	for i in cur_list:
-		f_out.write(str(i) + " ")  # writes each number, plus a tab
+	for count in cur_list:
+		f_out.write(str(count) + " ")  # writes each number, plus a tab
 	f_out.write("\n")  # writes a newline
 f_out.close() 
