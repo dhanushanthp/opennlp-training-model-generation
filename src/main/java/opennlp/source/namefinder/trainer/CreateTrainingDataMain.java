8fd92dd1680a73353c8817084d5e897b905de6f4
root <root@dhanu-HP-ProBook-4430s>
2015-07-14 14:27:32 +0530

    adding multiple jar file creation in pom.

--- src/main/java/opennlp/source/namefinder/trainer/CreateTrainingDataMain.java
+++ src/main/java/opennlp/source/namefinder/trainer/CreateTrainingDataMain.java
@@ -1,4 +1,4 @@
-package opennlp.source.chuncker.trainer;
+package opennlp.source.namefinder.trainer;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 
 import core.util.ReadTxtFile;
 import core.util.WriteFile;
-import opennlp.source.namefinder.trainer.CreateTrainNameTrainData;
 import opennlp.source.sentencer.SentenceDetector;
 
 public class CreateTrainingDataMain {
