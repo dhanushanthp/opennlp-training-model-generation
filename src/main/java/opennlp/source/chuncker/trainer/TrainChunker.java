cb589061af984fefd72fc467d7d7fa45b9a058e7
root <root@dhanu-HP-ProBook-4430s>
2015-07-15 09:38:14 +0530

    building model for chunker using train data.

--- src/main/java/opennlp/source/chuncker/trainer/TrainChunker.java
+++ src/main/java/opennlp/source/chuncker/trainer/TrainChunker.java
@@ -7,7 +7,11 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import core.util.Config;
+import core.util.FileUtils;
 import opennlp.tools.chunker.ChunkSample;
 import opennlp.tools.chunker.ChunkSampleStream;
 import opennlp.tools.chunker.ChunkerME;
@@ -18,31 +22,35 @@ import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.TrainingParameters;
 
 public class TrainChunker {
+	private static final Logger LOG = LoggerFactory.getLogger(TrainChunker.class);
 
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException {
+		LOG.debug("loading train data from : " + Config.getTrainDataPath() + "en-chunker.train");
+		LOG.debug("writing trained model to  : " + Config.getModelDataPath() + "en-chunker.bin");
+
+		FileUtils.CreateMultiDirec(Config.getModelDataPath());
+
 		Charset charset = Charset.forName("UTF-8");
-		ObjectStream<String> lineStream =
-		    new PlainTextByLineStream(new FileInputStream(Config.getTrainDataPath() + "en-chunker.train"),charset);
-		ObjectStream<ChunkSample> sampleStream = new ChunkSampleStream(lineStream);
+		ObjectStream<String> lineStream = new PlainTextByLineStream(new FileInputStream(Config.getTrainDataPath() + "en-chunker.train"),
+				charset);
+		ObjectStream<ChunkSample> objectStream = new ChunkSampleStream(lineStream);
 
 		ChunkerModel model;
 
 		try {
-		  model = ChunkerME.train("en", sampleStream,
-		      new DefaultChunkerContextGenerator(), TrainingParameters.defaultParams());
-		}
-		finally {
-		  sampleStream.close();
+			model = ChunkerME.train("en", objectStream, new DefaultChunkerContextGenerator(), TrainingParameters.defaultParams());
+		} finally {
+			objectStream.close();
 		}
 
 		OutputStream modelOut = null;
 		try {
-		  modelOut = new BufferedOutputStream(new FileOutputStream("src/main/resources/opennlp/my-chunker.bin"));
-		  model.serialize(modelOut);
+			modelOut = new BufferedOutputStream(new FileOutputStream(Config.getModelDataPath() + "en-chunker.bin"));
+			model.serialize(modelOut);
 		} finally {
-		  if (modelOut != null)
-		     modelOut.close();
+			if (modelOut != null)
+				modelOut.close();
 		}
 
 	}
