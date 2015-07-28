fc869b7db5924a6f6ec93f97da93ccaf610b036e
Dhanushanth <dhanushanthp@gmail.com>
2015-07-28 10:40:36 +0530

    for now all tokenizer and pos tagger tasks done.

--- src/main/java/opennlp/source/pos/executor/PosTagger.java
+++ src/main/java/opennlp/source/pos/executor/PosTagger.java
@@ -1,22 +1,18 @@
 package opennlp.source.pos.executor;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
-
+import core.util.Config;
 import opennlp.source.tokenizer.executor.Tokenizer;
-import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
-import opennlp.tools.tokenize.WhitespaceTokenizer;
 
 public class PosTagger {
 	public static POSObj POSTag(String input) throws IOException {
-		POSModel model = new POSModelLoader().load(new File("en-pos-maxent.bin"));
+		POSModel model = new POSModel(new FileInputStream(Config.getModelDataPath() + "en-pos.bin"));
 		POSTaggerME tagger = new POSTaggerME(model);
 
-//		String tokenWords[] = WhitespaceTokenizer.INSTANCE.tokenize(input);
 		String tokenWords[] = Tokenizer.getTokens(input);
 		String[] tags = tagger.tag(tokenWords);
 		POSSample result = new POSSample(tokenWords, tags);
@@ -26,7 +22,7 @@ public class PosTagger {
 	}
 
 	public static void main(String[] args) throws IOException {
-		POSObj resu = POSTag("“James B Stewart“ Common Sense column observes Apple, formerly market laggard, has far distanced Microsoft in share price since January 2014");
+		POSObj resu = POSTag("“James B Stewart“ Common Sense column observes Apple, formerly market laggard, has far distanced Microsoft in share price since January 2014.");
 //		System.out.println(Arrays.toString(resu.getTags()));
 //		System.out.println(Arrays.toString(resu.getTokens()));
 	}
