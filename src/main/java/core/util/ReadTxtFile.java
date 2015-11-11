2bf57c1d0b3d3f725484ca8f672c047bd809eb54
root <root@dhanu-HP-ProBook-4430s>
2015-11-11 16:56:21 +0530

    adding text extraction from xml.

--- src/main/java/core/util/ReadTxtFile.java
+++ src/main/java/core/util/ReadTxtFile.java
@@ -4,6 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collection;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * @author Dhanushanth
  * Extract text from a text file and do some text processing to clean the string.
@@ -37,15 +44,22 @@ public class ReadTxtFile {
 		String line = null;
 
 		StringBuffer sb = new StringBuffer();
+		StringBuffer pageContent = new StringBuffer();
 		try {
 			FileReader fileReader = new FileReader(fileName);
 
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 
 			while ((line = bufferedReader.readLine()) != null) {
-				if(!line.contains("</doc>") && !line.contains("<doc id")){
 					sb.append(line + " ");
-				}
+			}
+			
+			Document doc = Jsoup.parse(sb.toString());
+			Elements pages = doc.select("doc");
+
+			for (Element page : pages) {
+				String content = page.text();
+				pageContent.append(content);
 			}
 
 			bufferedReader.close();
@@ -55,7 +69,7 @@ public class ReadTxtFile {
 			System.out.println("Error reading file '" + fileName + "'");
 		}
 
-		return textCleaning(sb.toString());
+		return textCleaning(pageContent.toString());
 	}
 
 	public static String textCleaning(String input) {
